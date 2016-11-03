e5c0cdb467e58e1c7fca2a5f8f3a5f9c330e3325
William Terry <Tezza48is@aol.com>
2016-11-03 22:50:41 +0000

    Trying to create D3D Device

--- RenderingEngine/Main.cpp
+++ RenderingEngine/Main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
 #include <windows.h>
+#include <wrl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+//#include "d3dx12.h"// from 
+
+// dont forget to set Target Platform Version to '10.x' p.xxxiii (Luna)
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+using namespace std;
+using namespace Microsoft::WRL;
+using namespace DirectX;
 
 static TCHAR szWindowClass[] = _T("Rendering Engine");// the name of the application
 static TCHAR szTitle[] = _T("DirectX 12 powered Rendering Engine");
@@ -45,34 +59,30 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	hInst = hInstance;
 
 	// Now we can create the window
-	HWND hWnd = CreateWindow(
-		szWindowClass,
-		szTitle,
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		500, 100,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-		);
+	HWND hWnd = CreateWindow(szWindowClass, szTitle,
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 100, NULL, NULL, hInstance, NULL);
 
 	if (!hWnd)
 	{
-		MessageBox(
-			NULL,
-			_T("Call to CreateWindow failed!"),
-			_T("DX12 Rendering Engine"),
-			NULL
-			);
+		MessageBox(NULL, _T("Call to CreateWindow failed!"), _T("DX12 Rendering Engine"), NULL);
 
 		return 1;
 	}
+
 
 	// now display the window
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
+	// Create the Device
+#if defined(DEBUG) || defined(_DEBUG)
+	// Enable D3D12 debug layer
+	ComPtr<ID3D12Debug> debugController;
+
+#endif // defined(DEBUG) || defined(_DEBUG)
+	HRESULT hardwareResult = D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS)
+
+	// Set up Message Loop
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
